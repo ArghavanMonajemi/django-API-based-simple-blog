abaff29574d054cda992fbef56ea2e146b7f9963
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-10 16:37:29 +0330

    - project created. - 2 apps added to the project: - blog - account

---
+++ core/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Post(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='posts/%Y/%m/%d', blank=True, null=True)
+    status = models.BooleanField(default=False)
+    update_date = models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
