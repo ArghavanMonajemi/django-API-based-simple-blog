3bf44d3b0649439ebc9ae0505a3749a9ad090901
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-10 16:42:11 +0330

    - custom User Model created.

---
+++ core/account/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from core.account.managers import UserManager
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
