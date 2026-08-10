08c2a8e78e4417f11050f2970dc9e799675ecae6
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-10 17:16:16 +0330

    - User config in App setting changed. - Wrong import error fixed in the account.models.py. - Custom userAdmin added to the project.

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email','is_staff','is_superuser','is_active','created_date')
+    list_filter = ('email','is_staff','is_superuser','is_active','created_date')
+    fieldsets = (
+        ('Authentication', {'fields': ('email', 'password')}),
+        ("Permissions", {'fields': ("is_staff", "is_active", "is_superuser")}),
+        ('Groups Permissons', {"fields": ("groups",'user_permissions')}),
+        ('Important Dates', {"fields": ("last_login", "created_date")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff"
+            )}
+         ),
+    )
+    ordering = ('email',)
+
+admin.site.register(User,CustomUserAdmin)
--- core/account/models.py
+++ core/account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-from core.account.managers import UserManager
+from .managers import UserManager
 
 
 class User(AbstractBaseUser,PermissionsMixin):
