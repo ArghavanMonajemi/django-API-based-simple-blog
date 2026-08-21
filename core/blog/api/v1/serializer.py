31c9bf62f99bbeb9275efe02aff9a7c520d6f201
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-22 01:27:44 +0330

    - PostSerialaizer add

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -1,7 +1,29 @@
-from blog.models import Category
+from blog.models import Category, Post
+from account.models import Profile
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        category = CategorySerializer()
+        fields = ('id','author','image', 'title', 'snippet','content', 'category', 'status', 'create_date', 'update_date', 'pub_date')
+        read_only_fields = ('author', 'snippet')
+
+    def create(self, validated_data):
+        validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+        return super().create(validated_data)
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        representation = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            representation.pop('snippet')
+        else:
+            representation.pop('content')
+        representation['category'] = CategorySerializer(instance.category,context = {'request': request}).data
+        return representation
