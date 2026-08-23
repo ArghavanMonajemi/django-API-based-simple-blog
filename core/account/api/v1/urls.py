67795055bd2b24ee4a2dab5c44206f2be7ac7756
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-24 03:18:42 +0330

    - Token Authentication login add with custom Serializer and View

--- core/account/api/v1/urls.py
+++ core/account/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, CustomAuthToken
 
 app_name = 'account-api-v1'
 
 urlpatterns = [
     path('registration/',RegistrationView.as_view(),name='registration'),
+    path('token/login/',CustomAuthToken.as_view(),name='token-login'),
 ]
 
 
--- core/account/api/v1/views.py
+++ core/account/api/v1/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
-from .serializer import RegistrationSerializer
-from rest_framework.response import Response
+from .serializer import RegistrationSerializer, CustomAuthTokenSerializer
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 
 class RegistrationView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
@@ -15,3 +18,19 @@ class RegistrationView(generics.GenericAPIView):
             }
             return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
