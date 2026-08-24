28bfc66f200062e8649c15d1c598584995c7ec5f
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-24 13:36:12 +0330

    - JWT create, refresh and verify url add

--- core/account/api/v1/urls.py
+++ core/account/api/v1/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import RegistrationView, CustomAuthToken, CustomDiscardToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 app_name = 'account-api-v1'
 
@@ -7,4 +12,8 @@ urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('token/login/', CustomAuthToken.as_view(), name='token-login'),
     path('token/logout/', CustomDiscardToken.as_view(), name='token-logout'),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-create'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-create'),
+
 ]
