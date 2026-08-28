cb707436347645064a860bce5e1b7196bd61eda2
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-29 01:07:04 +0330

    - settings.py update

--- core/core/settings.py
+++ core/core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY",default="Test")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default="True"))
+DEBUG = bool(os.environ.get("DEBUG", default="False"))
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
