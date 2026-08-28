3be9969e0e2c55505b11c0a61bf13af5264f8d2d
Arghavan Monajemi <monajemi.am@gmail.com>
2026-08-29 01:44:43 +0330

    - add PostAdmin class

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post, Category
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author','create_date')
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
